bdfccb3a8bc703ddb613883c28fa5180f94ffdca
Caio Caminha <caio.caminha@globalpay.com>
2024-11-22 06:23:06 -0300

    hashset

---
+++ src/main/kotlin/com/caminha/leetcode/HashMapsAndSets/JewelsAndStones.kt
@@ -0,0 +1,21 @@
+package com.caminha.leetcode.HashMapsAndSets
+
+class JewelsAndStones {
+
+    fun numJewelsInStones(jewels: String, stones: String): Int {
+        var count = 0
+        val jewelsMap = HashSet<Char>()
+
+        for(char in jewels){
+            jewelsMap.add(char)
+        }
+
+        for(char in stones) {
+            if (jewels.contains(char)) count++
+        }
+
+        return count
+
+    }
+
+}
